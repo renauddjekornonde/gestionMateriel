54118776a9d571c7025af0436c05be7578a2fe8e
Aicha <Aicha@gmail.com>
2022-08-31 09:36:12 +0000

    insertion du template dans le projet

---
+++ app/Http/Controllers/AcceuilController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AcceuilController extends Controller
+{
+    //le controller retourne la vue de notre page d'accueil 'app'
+    public function index()
+    {
+        return view('layout.app');
+    }
+}
