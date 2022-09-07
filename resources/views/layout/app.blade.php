0b22b60e219a05efe115e78937e4c3776bacb91b
renauddjekornonde <renaud.djekornonde@supdeco.edu.sn>
2022-09-07 17:11:07 +0000

    modification de create

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -121,6 +121,9 @@
     <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
     <link rel="stylesheet" href="{{asset('asset/style.css')}}">
 
+{{-- ============Bootstrap========== --}}
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous"> 
+
 
 </head>
 <body>
@@ -158,7 +161,8 @@
 <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 <!-- =========================End ionicon================= -->
 
-
+        <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 
 
 </body>
--- resources/views/materiel/create.blade.php
+++ resources/views/materiel/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.app')
 
 @section('content')
-                              <center>
+                              {{-- <center>
         <fieldset style="width: 70%;">
             <form action="{{route('materiel.store')}}" method="POST">
                 <h1>Ajouter Materiel</h1>
@@ -31,11 +31,29 @@
                 </table>
             </form>
         </fieldset>
-    </center>
-                    </div>
-                          
+    </center> --}}
+                      <form>
+            <fieldset>
+              <legend>Disabled fieldset example</legend>
+              <div class="mb-3">
+                <label for="disabledTextInput" class="form-label">Disabled input</label>
+                <input type="text" id="disabledTextInput" class="form-control" placeholder="Disabled input">
+              </div>
+              <div class="mb-3">
+                <label for="disabledSelect" class="form-label">Disabled select menu</label>
+                <select id="disabledSelect" class="form-select">
+                  <option>Disabled select</option>
+                </select>
+              </div>
+              <div class="mb-3">
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" id="disabledFieldsetCheck" disabled>
+                  <label class="form-check-label" for="disabledFieldsetCheck">
+                    Can't check this
+                  </label>
                 </div>
-             </div>
-         </div>
-    </div>
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </fieldset>
+          </form>
 @endsection
